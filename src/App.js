44b26d582d28fee63237174f06ae18435e9a9460
jinyoung <wlsud801@gmail.com>
2023-04-26 00:14:12 +0900

    add img, css, html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/logo.png'
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <img className="App-logo" src={logo} alt="주사위게임 로고" />
+        <h1 className="App-title">주사위게임</h1>
+        <div>
+          <button className="Button blue App-button">던지기</button><button className="Button red App-button">처음부터</button>
+        </div>
+      </div>
+      <div className="App-boards">
+        <div className="Board App-board">
+          <h2 className="Board-heading">나</h2>
+          <img className="Dice" src="dice-blue-1.svg" alt="1" />
+          <h2 className="Board-heading">기록</h2>
+          <p></p>
+          <h2 className="Board-heading">총점</h2>
+          <p>0</p>
+        </div>
+        <div className="Board App-board">
+          <h2 className="Board-heading">상대</h2>
+          <img className="Dice" src="dice-red-1.svg" alt="1" />
+          <h2 className="Board-heading">기록</h2>
+          <p></p>
+          <h2 className="Board-heading">총점</h2>
+          <p>0</p>
+        </div>
+      </div>
+    </>
   );
 }
 
